8f13335de94af4bf9b98c36268dbe56c6cec71e6
kwon, minho <minhob38@gmail.com>
2022-07-17 03:20:48 +0900

    add head for viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import styled from "@emotion/styled";
 import { wrapper } from "../store";
 import "../assets/styles/globals.css";
@@ -12,6 +13,12 @@ const MobileWrapper = styled.a`
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MobileWrapper>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1,  minimum-scale=1.0, user-scalable=no, shrink-to-fit=no"
+        />
+      </Head>
       <Component {...pageProps} />
     </MobileWrapper>
   );
