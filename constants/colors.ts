506e32451d961bb8a17e466aa5b7de03243db0dc
kwon, minho <minhob38@gmail.com>
2022-07-29 00:04:56 +0900

    add color

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,9 +1,14 @@
 export const PRIMARY = '#b8f5f5';
+
 export const BLACK1 = '#1a1a1a';
 export const BLACK2 = '#4c4c4c';
+export const BLACK3 = '#3f3f3f';
+
 export const GRAY1 = '#999999';
 export const GRAY2 = '#f2f2f2';
+
 export const WHITE1 = '#ffffff';
 export const WHITE2 = '#fafafa';
+
 export const YELLOW1 = '#f3bc4e';
 export const YELLOW2 = '#e4813a';
