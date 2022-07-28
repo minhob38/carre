c234f2c241c376ec633dd2a263f200070e5fa995
kwon, minho <minhob38@gmail.com>
2022-07-29 00:05:21 +0900

    add brand variables

---
+++ constants/variables.ts
@@ -0,0 +1,57 @@
+export {};
+
+interface IBrands {
+  title: string;
+  value: string;
+}
+
+const brands: IBrands[] = [
+  {
+    title: '현대',
+    value: 'hyundai',
+  },
+  {
+    title: 'bmw',
+    value: 'bmw',
+  },
+  {
+    title: '벤츠',
+    value: 'benz',
+  },
+  {
+    title: '기아',
+    value: 'kia',
+  },
+  {
+    title: '르노 삼성',
+    value: 'renault-samsung',
+  },
+  {
+    title: '렉서스',
+    value: 'lexus',
+  },
+  {
+    title: '쉐보레',
+    value: 'chevrolet',
+  },
+  {
+    title: '도요타',
+    value: 'toyota',
+  },
+  {
+    title: '혼다',
+    value: 'honda',
+  },
+  {
+    title: '폭스바겐',
+    value: 'volkswagen',
+  },
+  {
+    title: '아우디',
+    value: 'audi',
+  },
+  {
+    title: '포르쉐',
+    value: 'porche',
+  },
+];
