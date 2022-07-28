04b23e8e44211e5048dd3448c300c1cf6a06bcc6
kwon, minho <minhob38@gmail.com>
2022-07-29 00:28:35 +0900

    remove unneccessary code

--- pages/test/3.tsx
+++ pages/test/3.tsx
@@ -3,19 +3,17 @@ import { useState } from 'react';
 import type { NextPage } from 'next';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { v4 as uuid4 } from 'uuid';
 import InputLabel from '@components/InputLabel';
 import NextButton from '@components/NextButton';
 import ProgressBar from '@components/ProgressBar';
 import Canvas from '@components/Canvas';
 import Header from '@components/Header';
 import Image from '@components/Image';
-import checkImage from '@assets/images/check.svg';
-import * as colors from '@constants/colors';
-import Budget from '@components/Budget';
-import brandImage from '@assets/images/brand.svg';
 import Arrow from '@components/Arrow';
+import * as colors from '@constants/colors';
 import { brands } from '@constants/variables';
-import { v4 as uuid4 } from 'uuid';
+import brandImage from '@assets/images/brand.svg';
 
 const Title = styled.div`
   margin: 30px 0 2px 0;
@@ -29,68 +27,6 @@ const Description = styled.div`
   color: ${colors.GRAY1};
 `;
 
-const SubTitle = styled.div`
-  margin: 0;
-  font: normal 400 16px / 27px roboto;
-  color: ${colors.BLACK2};
-`;
-
-const SubDescription = styled.div`
-  margin: 0 0 30px 0;
-  font: normal 400 12px / 20px roboto;
-  color: ${colors.GRAY1};
-`;
-
-const SelectContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100px;
-  height: 48px;
-  border-radius: 20px;
-  background: #ffffff;
-  box-shadow: 0px 4.43038px 9.72px rgba(96, 100, 112, 0.06);
-`;
-
-const Select = styled.select`
-  all: unset;
-  flex: 1;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  text-align: right;
-  font: normal 500 15px / 21px roboto;
-  color: #7a7979;
-`;
-
-const ArrowBox = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 40px;
-`;
-
-const YearContainer = styled.div`
-  margin: 0 35px 0 0;
-`;
-
-const GenderContainer = styled.div``;
-
-const PurposeContainer = styled.div`
-  margin: 40px 0 0 0;
-`;
-
-const ImageContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 35px;
-  height: 35px;
-`;
-
-const Div = styled.div``;
-const Ul = styled.ul``;
 const Test: NextPage = () => {
   const [touchBudgetX, setTouchBudgetX] = useState<number>(0);
   const [budgetX, setBudgetX] = useState<number>(55);
