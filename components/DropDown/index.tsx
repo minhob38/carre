a695e4d57bb34d7c41612151d1823c5ed4d9def4
kwon, minho <minhob38@gmail.com>
2022-07-29 01:06:54 +0900

    refactor dropdown component

---
+++ components/DropDown/index.tsx
@@ -0,0 +1,85 @@
+/** @jsxImportSource @emotion/react */
+import { useState } from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import Image from '@components/Image';
+import Arrow from '@components/Arrow';
+import * as colors from '@constants/colors';
+import brandImage from '@assets/images/brand.svg';
+
+interface IProps {
+  title: string;
+}
+
+interface IStyleProps {
+  isDown: boolean;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  width: calc(100% - 20px - 20px);
+  border-radius: 8px;
+  background-color: ${colors.GRAY2};
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  width: calc(100% - 20px - 20px);
+  height: 70px;
+  margin: auto;
+  border-bottom: solid 1px ${colors.GRAY3};
+`;
+
+const Title = styled.div`
+  margin: 0 0 0 16px;
+  font: normal 400 16px / 27px roboto;
+  color: ${colors.BLACK3};
+`;
+
+const ItemContainer = styled.div`
+  display: ${(props: IStyleProps) => (props.isDown ? 'flex' : 'none')};
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 9px 14px;
+  padding: 14px 0;
+`;
+
+const DropDown: React.FC<IProps> = ({ children, title }) => {
+  const [isDown, setIsDown] = useState<boolean>(false);
+
+  return (
+    <Wrapper>
+      <Header>
+        <Image src={brandImage} alt="brand" width="30px" height="30px" />
+        <Title>{title}</Title>
+        <div
+          css={css`
+            flex: 1;
+          `}
+        />
+        <Arrow
+          length="14px"
+          width="2px"
+          color={colors.BLACK2}
+          direction={isDown ? 'bottom' : 'top'}
+          calibrationY="2.5px"
+          onClick={() => setIsDown(!isDown)}
+        />
+        <div
+          css={css`
+            width: 20px;
+          `}
+        />
+      </Header>
+      <ItemContainer isDown={isDown}>{children}</ItemContainer>
+    </Wrapper>
+  );
+};
+
+export default DropDown;
--- pages/test/3.tsx
+++ pages/test/3.tsx
@@ -14,6 +14,7 @@ import Arrow from '@components/Arrow';
 import * as colors from '@constants/colors';
 import { brands } from '@constants/variables';
 import brandImage from '@assets/images/brand.svg';
+import DropDown from '@components/DropDown';
 
 const Title = styled.div`
   margin: 30px 0 2px 0;
@@ -29,8 +30,7 @@ const Description = styled.div`
 
 const Test: NextPage = () => {
   const [isBrandDown, setIsBrandDown] = useState<boolean>(false);
-  const [budgetX, setBudgetX] = useState<number>(55);
-  console.log(brands);
+
   const Brands = brands.map((brand) => {
     return (
       <InputLabel
@@ -56,75 +56,7 @@ const Test: NextPage = () => {
         <Title>추가 선택 조건 선택</Title>
         <Description>꼭 원하는 조건이 있다면선택해주세요</Description>
       </Canvas>
-      <div
-        css={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        `}
-      >
-        <div
-          css={css`
-            width: calc(100% - 20px - 20px);
-            border-radius: 8px;
-            background-color: ${colors.GRAY2};
-          `}
-        >
-          <div //헤더
-            css={css`
-              display: flex;
-              align-items: center;
-              width: calc(100% - 20px - 20px);
-              height: 70px;
-              margin: auto;
-              border-bottom: solid 1px ${colors.GRAY3};
-            `}
-          >
-            <Image src={brandImage} alt="brand" width="30px" height="30px" />
-            <div
-              css={css`
-                margin: 0 0 0 16px;
-                font: normal 400 16px / 27px roboto;
-                color: ${colors.BLACK3};
-              `}
-            >
-              브랜드
-            </div>
-            <div
-              css={css`
-                flex: 1;
-              `}
-            />
-            <Arrow
-              length="14px"
-              width="2px"
-              color={colors.BLACK2}
-              direction={isBrandDown ? 'bottom' : 'top'}
-              calibrationY="2.5px"
-              onClick={() => setIsBrandDown(!isBrandDown)}
-            />
-            <div
-              css={css`
-                width: 20px;
-              `}
-            />
-          </div>
-
-          <div
-            css={css`
-              display: flex;
-              display: ${isBrandDown ? 'flex' : 'none'};
-              flex-flow: row wrap;
-              justify-content: center;
-              align-items: center;
-              gap: 9px 14px;
-              padding: 14px 0;
-            `}
-          >
-            {Brands}
-          </div>
-        </div>
-      </div>
+      <DropDown title="브랜드">{Brands}</DropDown>
       <ProgressBar stage={3} />
       <NextButton path={'/test/3'} />
     </>
