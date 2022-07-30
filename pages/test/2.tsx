a6c4417ac660fcbb0023a68de8e0bf92d0dc4ef8
kwon, minho <minhob38@gmail.com>
2022-07-30 20:47:00 +0900

    refactor page2

--- pages/test/2.tsx
+++ pages/test/2.tsx
@@ -5,6 +5,7 @@ import styled from '@emotion/styled';
 import NextButton from '@components/NextButton';
 import ProgressBar from '@components/ProgressBar';
 import Header from '@components/Header';
+import Content from '@components/Content';
 import Image from '@components/Image';
 import Budget from '@components/Budget';
 import checkImage from '@assets/images/check.svg';
@@ -38,37 +39,39 @@ const Test: NextPage = () => {
   return (
     <>
       <Header title="나의 정보 입력" />
-      <Title>나의 구매 필수 조건 입력</Title>
-      <Description>차량 구매에 필수적인 나의 정보를 입력해요</Description>
-      <SubTitle>가격설정</SubTitle>
-      <SubDescription>가격의 스펙트럼을 변경해요</SubDescription>
-      <div
-        css={css`
-          display: flex;
-          margin: 0 0 0 18px;
-        `}
-      ></div>
-      <Budget />
-      <div
-        css={css`
-          display: flex;
-          align-items: center;
-          margin: 15px 0 0 35px;
-        `}
-      >
-        <Image src={checkImage} alt="check" width="18px" height="18px" />
+      <Content top="55px" bottom="0">
+        <Title>나의 구매 필수 조건 입력</Title>
+        <Description>차량 구매에 필수적인 나의 정보를 입력해요</Description>
+        <SubTitle>가격설정</SubTitle>
+        <SubDescription>가격의 스펙트럼을 변경해요</SubDescription>
         <div
           css={css`
-            font: normal 400 13px / 26px roboto;
-            color: ${colors.GRAY1};
-            margin: 0 0 0 16px;
+            display: flex;
+            margin: 0 0 0 18px;
+          `}
+        ></div>
+        <Budget />
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            margin: 15px 0 0 35px;
           `}
         >
-          비슷한 가격대 차량도 함께 보기
+          <Image src={checkImage} alt="check" width="18px" height="18px" />
+          <div
+            css={css`
+              font: normal 400 13px / 26px roboto;
+              color: ${colors.GRAY1};
+              margin: 0 0 0 16px;
+            `}
+          >
+            비슷한 가격대 차량도 함께 보기
+          </div>
         </div>
-      </div>
-      <ProgressBar stage={2} />
-      <NextButton path={'/test/3'} />
+        <ProgressBar stage={2} />
+        <NextButton path={'/test/3'} />
+      </Content>
     </>
   );
 };
