e9b98c7a79c8525de76e7866ab87425b40b2ccbd
kwon, minho <minhob38@gmail.com>
2022-08-06 00:40:33 +0900

    implement main test page

---
+++ components/Banner/index.tsx
@@ -0,0 +1,58 @@
+/** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled';
+import Image from '@components/Image';
+import * as colors from '@constants/colors';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: calc(100% - 20px - 20px);
+  height: 194px;
+  background: #ffffff;
+  box-shadow: 0px 5px 20px rgba(96, 100, 112, 0.04);
+  border-radius: 8px;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const Title = styled.div`
+  font: normal 600 16px / 27px Roboto;
+  color: ${colors.BLACK1};
+  white-space: pre;
+`;
+
+const Description = styled.div`
+  font: normal 400 14px / 20px Roboto;
+  color: ${colors.GRAY1};
+  white-space: pre;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 145px;
+`;
+
+const Banner: React.FC<{ data: any }> = ({ data }) => {
+  const { image, title, description } = data;
+  const { src, alt, width, height } = image;
+  return (
+    <Wrapper>
+      <ImageContainer>
+        <Image src={src} alt={alt} width={width} height={height} />
+      </ImageContainer>
+      <TextContainer>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+      </TextContainer>
+    </Wrapper>
+  );
+};
+
+export default Banner;
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,67 +1,85 @@
 /** @jsxImportSource @emotion/react */
 import type { NextPage } from 'next';
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import Header from '@components/Header';
+import { v4 as uuid4 } from 'uuid';
 import Content from '@components/Content';
 import * as colors from '@constants/colors';
-import Arrow from '@components/Arrow';
 import Link from 'next/link';
+import Banner from '@components/Banner';
+import Scroll from '@components/Scroll';
+import bannerAImage from '@assets/images/banner-a.svg';
+import bannerBImage from '@assets/images/banner-b.svg';
 
 const Title = styled.div`
-  margin: 24px 0 34px 27px;
-  font: normal 400 20px / 31px roboto;
-  color: ${colors.BLACK4};
-`;
-
-const ItemText = styled.div`
-  font: 'normal 400 18px / 20px roboto';
+  margin: 45px 0 0 20px;
+  font: normal 600 22px / 35px roboto;
   color: ${colors.BLACK1};
+  white-space: pre;
 `;
 
-const TestItemContainer = styled.div`
+const DescriptionContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-flow: column;
+  gap: 4px 0;
+  margin: 98px 0 0 30px;
+`;
+
+const Description = styled.div`
+  font: normal 600 12px / 20px roboto;
+  color: ${colors.BLACK2};
+`;
+
+const BannerContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  gap: 20px 0;
   align-items: center;
-  width: 350px;
-  height: 78px;
-  border-radius: 8px;
-  background-color: ${colors.YELLOW1};
-  padding: 0 23px;
+  margin: 48px 0 0 0;
 `;
 
 const Test: NextPage = () => {
-  const genderLabels: { title: string; value: string }[] = [
-    { title: '여자', value: 'female' },
-    { title: '남자', value: 'male' },
+  const banners = [
+    {
+      image: {
+        src: bannerAImage,
+        alt: 'banner-a',
+        width: '83px',
+        height: '104px',
+      },
+      title: `검증된 데이터를 통한 \n신뢰성있는 추천`,
+      description: `구축된 데이터 베이스를 통해 \n전문 분석된 차량을 추천합니다.`,
+    },
+    {
+      image: {
+        src: bannerBImage,
+        alt: 'banner-b',
+        width: '104px',
+        height: '104px',
+      },
+      title: `고객의 우선순위 및 \n디테일한 취향 반영`,
+      description: `840여개 고객 유형을 통해 \n나의 우선순위와 취향을 반영합니다.`,
+    },
   ];
 
+  const Banners = banners.map((banner) => {
+    return <Banner key={uuid4()} data={banner} />;
+  });
+
   return (
-    <div>
-      <Header title="차량 구매 성향 테스트" backPath="/test" />
-      <Content top="55px" bottom="0">
-        {/* TODO: 변수로 사용자 이름 넣기 */}
-        <Title>성능과 안전 두마리 토끼를 잡으려는 카레님</Title>
-        <div
-          css={css`
-            margin: 0 0 0 20px;
-          `}
-        >
-          <Link href="/test/1" passHref={true}>
-            <TestItemContainer>
-              <ItemText>테스트하러가기</ItemText>
-              <Arrow
-                length="7px"
-                width="2px"
-                color={colors.BLACK2}
-                direction="right"
-                calibrationX="2.5px"
-              />
-            </TestItemContainer>
-          </Link>
-        </div>
-      </Content>
-    </div>
+    <Content top="0" bottom="72px">
+      <Scroll direction="y" height="100%">
+        <Title>
+          {`내 성향을 분석해서 \n나에게 딱 맞는 차를 찾아주는 \n똑똑한 차량 추천 서비스`}
+        </Title>
+        <DescriptionContainer>
+          <Description>#내 차량 구매 성향 분석</Description>
+          <Description>#나에게 Fit한 차량 추천</Description>
+          <Description>#내게 맞는 차량 검색</Description>
+          <Description>#전문 딜러에게 간단 견적요청</Description>
+        </DescriptionContainer>
+        <BannerContainer>{Banners}</BannerContainer>
+      </Scroll>
+    </Content>
   );
 };
 
