275776c5cbdb2b1fb883e72b3a3a75178acacd40
kwon, minho <minhob38@gmail.com>
2022-08-06 21:55:57 +0900

    update birth date values

--- pages/test/2.tsx
+++ pages/test/2.tsx
@@ -124,7 +124,26 @@ const Test: NextPage = () => {
             <SubTitle>출생연도</SubTitle>
             <SelectContainer>
               <Select>
-                <option value="1970">1970</option>
+                <option value="1980">1980</option>
+                <option value="1981">1981</option>
+                <option value="1982">1982</option>
+                <option value="1983">1983</option>
+                <option value="1984">1984</option>
+                <option value="1985">1985</option>
+                <option value="1986">1986</option>
+                <option value="1987">1987</option>
+                <option value="1988">1988</option>
+                <option value="1989">1989</option>
+                <option value="1990">1990</option>
+                <option value="1991">1991</option>
+                <option value="1992">1992</option>
+                <option value="1993">1993</option>
+                <option value="1994">1994</option>
+                <option value="1995">1995</option>
+                <option value="1996">1996</option>
+                <option value="1997" selected={true}>
+                  1997
+                </option>
               </Select>
             </SelectContainer>
           </YearContainer>
