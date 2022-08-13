2ea019979bd7af7edd20e58e1365934bc655d179
kwon, minho <minhob38@gmail.com>
2022-08-14 00:33:46 +0900

    update page7

--- pages/test/7.tsx
+++ pages/test/7.tsx
@@ -8,19 +8,22 @@ import NextButton from '@components/common/NextButton';
 import Content from '@components/common/Content';
 import ImageLabel from '@components/common/ImageLabel';
 import * as colors from '@constants/colors';
+import * as fonts from '@constants/fonts';
+import * as margins from '@constants/margins';
+import { HEADER_HEIGHT, NEXT_BUTTON_HEIGHT } from '@constants/size';
 import { styles } from '@constants/variables';
 
 const Title = styled.div`
-  margin: 20px 0 2px 20px;
-  font: normal 400 20px / 32px roboto;
-  color: ${colors.BLACK1};
+  margin: 28px 0 4px ${margins.SIDE_MAIN_MARGIN};
+  font: ${fonts.TITLE_T1};
+  color: ${colors.SECONDARY_500};
   white-space: pre;
 `;
 
 const Description = styled.div`
-  margin: 0 0 0 20px;
-  font: normal 400 14px / 23px roboto;
-  color: ${colors.GRAY1};
+  margin: 0 0 27px ${margins.SIDE_MAIN_MARGIN};
+  font: ${fonts.SUBTITLE_T1};
+  color: ${colors.SECONDARY_300};
   white-space: pre;
 `;
 
@@ -28,46 +31,42 @@ const StyleContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
   align-items: flex-start;
-  gap: 8px 0;
-  margin: 16px 0 0 30px;
+  gap: 10px 0;
+  margin: 16px 0 0 ${margins.SIDE_SUB_MARGIN};
+`;
+
+const ImageLabelContainer = styled.div`
+  box-shadow: 0px 4.43038px 9.72px rgba(96, 100, 112, 0.06);
+  border-radius: 8px;
 `;
 
 const Test: NextPage = () => {
-  const MAX_PAGE = 21;
-  const [page, setPage] = useState<number>(0);
-  const handleImageClick = async (ev) => {
-    await new Promise((resolve, reject) => {
-      setTimeout(() => resolve(''), 1500);
-    });
-    setPage(page + 1);
-  };
-
   const Questions = styles.map((style) => {
     return (
-      <ImageLabel
-        key={uuid4()}
-        input={{
-          type: 'checkbox',
-          name: style.name,
-          value: style.value,
-        }}
-        style={{
-          width: '330px',
-          height: '105px',
-        }}
-        image={{
-          src: style.src,
-          alt: style.alt,
-        }}
-        onClick={handleImageClick}
-      />
+      <ImageLabelContainer key={uuid4()}>
+        <ImageLabel
+          input={{
+            type: 'checkbox',
+            name: style.name,
+            value: style.value,
+          }}
+          style={{
+            width: '330px',
+            height: '106px',
+          }}
+          image={{
+            src: style.src,
+            alt: style.alt,
+          }}
+        />
+      </ImageLabelContainer>
     );
   });
 
   return (
     <>
-      <Header title="차량 성향 테스트" backPath="/test/4" />
-      <Content top="55px" bottom="72px">
+      <Header title="차량 성향 테스트" type="close" closePath="/" />
+      <Content top={HEADER_HEIGHT} bottom={NEXT_BUTTON_HEIGHT}>
         <Title>차량 스타일 선택</Title>
         <Description>선호하는 차량 스타일을 선택해주세요.</Description>
         <StyleContainer>{Questions}</StyleContainer>
