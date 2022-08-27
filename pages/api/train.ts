5cccfe132fe41f693cdce5ecec4f1bfd328cd529
kwon, minho <minhob38@gmail.com>
2022-08-27 16:51:27 +0900

    fix error

--- pages/api/train.ts
+++ pages/api/train.ts
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import { ITrainApi } from "../../types/types";
+import type { NextApiRequest, NextApiResponse } from 'next';
+// import { ITrainApi } from '@src/types/types';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ITrainApi>) {
-  res.status(200).json({ emoji: "🚂" });
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+) {
+  res.status(200).json({ emoji: '🚂' });
 }
